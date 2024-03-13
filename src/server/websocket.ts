07c2197f41e1d2fe6bb08ca2dd9d4284308bb509
Fernando Lira <fernando@lira.etc.br>
2024-03-12 23:42:51 -0300

    chore: websocket quase pronto

--- src/server/websocket.ts
+++ src/server/websocket.ts
@@ -1,18 +1,52 @@
 import WebSocket, { WebSocketServer } from 'ws';
 
+type Protocol = "Controller" | "App";
+
 class NeukoWSS {
   private _wss: WebSocket.Server;
+  private _clients: Map<WebSocket, Protocol>
   constructor() {
     this._wss = new WebSocketServer({ port: 1337 });
+    this._clients = new Map();
   }
 
-  init() {
-    this._wss.on('connection', ws => {
-      ws.on('open', () => {
-        "Hello, client";
-      }); 
+  private _setupProtocol(protocol: string): Protocol {
+    return protocol === "App" ? protocol : "Controller";
+  }
 
-    });
+  private _onConnection(ws: WebSocket) {
+    this._clients.set(ws, this._setupProtocol(ws.protocol));
+    console.dir(this._clients);
+    ws.on("open", this._onOpen.bind(this, ws));
+    ws.on("message", this._onMessage.bind(this, ws));
+    ws.on("close", this._onClose.bind(this, ws));
+  }
+
+  private _onOpen(ws: WebSocket) {
+    ws.send(`Connected to the server as ${this._setupProtocol(ws.protocol)}.`);
+    console.dir(this._clients);
+  }
+
+  private _onMessage(ws: WebSocket, data: string) {
+    if(ws.protocol !== "Controller") {
+      return;
+    }
+    const forwardMessage = (protocol: Protocol, ws: WebSocket) => {
+      if (protocol === "App") {
+        ws.send(data);
+      }
+    }
+    this._clients.forEach(forwardMessage);
+  }
+
+  private _onClose(ws:WebSocket) {
+    this._clients.delete(ws);
+    console.dir(this._clients);
+  }
+
+
+  init() {
+    this._wss.on('connection', this._onConnection.bind(this));
     return this;
   }
 
